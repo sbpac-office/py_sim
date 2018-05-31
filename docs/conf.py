624da0349ad0dde90fb41e4eab57c178c40964ff
Kyunghan-PC\Kyunghan <kyunghah.min@gmail.com>
2018-05-31 17:01:34 +0900

    no message

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, 'C:\Users\Kyunghan\Documents')
+sys.path.insert(0, '..\..')
 
 
 # -- Project information -----------------------------------------------------
--- sim_test/__init__.py
+++ sim_test/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 sim test sub pakcage overview
-~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Author
 -------
